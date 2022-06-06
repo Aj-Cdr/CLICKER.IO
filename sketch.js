1949ff560814946d5311ffe03bdaa0d21e041e6f
SPABOI <95515701+SPABOI@users.noreply.github.com>
2022-06-06 15:03:31 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -40,7 +40,7 @@ function draw() {
   fill(255)
   text('Score:   ' + score, windowWidth / 2, 125);
 
-  button = createSprite(windowWidth / 2, windowHeight / 2, windowWidth / 2, windowWidth /3);
+  button = createSprite(windowWidth / 2, windowHeight / 2, windowWidth / 2, windowWidth /5);
   button.addImage(buttonpic)
   touchStarted()
   drawSprites()
